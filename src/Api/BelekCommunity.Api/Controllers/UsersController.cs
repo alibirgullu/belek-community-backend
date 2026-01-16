dee59c9e08feaab8f0554ca7992a0cc84e59db3d
alibirgullu <alibirgullu01@gmail.com>
2026-01-16 14:53:19 +0300

    Add users controller and basic API structure

---
+++ src/Api/BelekCommunity.Api/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BelekCommunity.Api.Models;
+
+namespace BelekCommunity.Api.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Users GET endpoint works");
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateUserRequest request)
+        {
+            return Ok(new
+            {
+                Message = "User create request received",
+                Email = request.Email
+            });
+        }
+
+    }
+}
---
+++ src/Api/BelekCommunity.Api/Models/PlatformUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BelekCommunity.Api.Models
+{
+    public class PlatformUser
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public Guid ExternalId { get; set; }
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string PasswordHash { get; set; } = null!;
+    }
+}
